03af714a89ee04ceeed281c748ec6b51f14038c8
Kiran Karenavar <144697454+KiranKarenavar@users.noreply.github.com>
2023-09-12 19:52:56 +0530

    Add files via upload

---
+++ Day 2 Task.py
@@ -0,0 +1,11 @@
+print("Question :- Swapping of two numbers without using third varible \n")
+
+a = 50
+b = 100
+
+print("a =", a , "And b =", b)
+print("\n After Swapping \n")
+
+a, b = b, a
+
+print("a =", a , "And b = ", b)
